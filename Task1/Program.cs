f71abd97a8007c23222c0290fa30c0f279ac4461
Alex Besenev <Besenev96@gmail.com>
2024-02-03 11:49:52 +0500

    add some X lesson1:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+// - создание комментария в коде
+// - Целые числа: (int - целые числа диапазоном +-2*10^31, (больше) long - +-2*10^63)
+// Вещественные (дробные) float +-1.5*10^-45 - 3.4*10^38, (большую дробную чсть)  double +-5*10^-324 - 1.7*10^308
+// Символьные - char 0...65535(код символа)
+// Логический - bool - true false
+int value = 100;
+Console.WriteLine(value);
+double drob =5.55;
+char Vscl = '!';
+bool b = true;
+Console.WriteLine(drob);
+Console.WriteLine(Vscl);
+Console.WriteLine(b);
